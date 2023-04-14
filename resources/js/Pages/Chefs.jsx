e0b362b1dc0e04c8b21bd8e213ae9d1c3769de21
Jordan brierley <jord.brierley@gmail.com>
2023-04-14 15:55:03 +0100

    Initial Test Structure

---
+++ resources/js/Pages/Chefs.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, Head } from "@inertiajs/inertia-react";
+
+export default function Chefs(props) {
+    return (
+        <>
+            <Head title="Menus" />
+            <h1>Chefs</h1>
+        </>
+    );
+}
---
+++ resources/js/Pages/Menus.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link, Head } from "@inertiajs/inertia-react";
+
+export default function Menus(props) {
+    return (
+        <>
+            <Head title="Menus" />
+            <h1>Menus</h1>
+        </>
+    );
+}
